00cbd6d4d93c339da50be4dc120ea7bfbfcdf665
lekcik <cultcrib@gmail.com>
2024-09-22 23:31:57 +0200

    local storage added

--- myCook/src/App.jsx
+++ myCook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from 'react';
+import { useState, useEffect, createContext } from 'react';
 import Input from './components/input components/Input';
 import Header from './components/header components/Header';
 import './root.css';
@@ -7,14 +7,23 @@ import './general.css';
 export const ReceiptContext = createContext(); 
 
 function App() {
-  const [receipt, setReceipt] = useState({
-    products: [],
-    excludeProducts: [],
-    cuisine: '',
-    excludeCuisine: '',
-    diet: '',
-    instructionsRequired: false
+  const [receipt, setReceipt] = useState(() => {
+    const savedReceipt = localStorage.getItem('receipt');
+    return savedReceipt 
+      ? JSON.parse(savedReceipt) 
+      : {
+          products: [],
+          excludeProducts: [],
+          cuisine: '',
+          excludeCuisine: '',
+          diet: '',
+          instructionsRequired: false
+        };
   });
+
+  useEffect(() => {
+    localStorage.setItem('receipt', JSON.stringify(receipt));
+  }, [receipt]);
 
   return (
     <ReceiptContext.Provider value={[receipt, setReceipt]}>
