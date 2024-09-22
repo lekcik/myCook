8582f9d34afceff5cdc5639b89ddb3fe7e0d21b9
lekcik <cultcrib@gmail.com>
2024-09-22 13:12:45 +0200

    input functionality added

--- myCook/src/components/input components/Input.jsx
+++ myCook/src/components/input components/Input.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
 
 function Input() {
+    const [products, setProducts] = useState([]); 
+    const [inputValue, setInput] = useState('');
+
+    function inputChangeHandler(event) {
+        setInput(event.target.value);
+    }
+
+    function addProduct() {
+        if (inputValue !== '') {
+            setProducts((p) => [...p, inputValue]);
+            setInput('');
+        }
+    }
+
+    function removeProduct(index) {
+        let temp = products.filter((_, i) => 
+            i !== index
+        );
+        setProducts(temp);
+    }
+
     return(
-        <section className="input container" aria-label="Details about your producst">
+        <section className="input container" aria-label="Details about your products">
             <h2>What's in your fridge today?</h2>
+            <div>
+                <input value={inputValue} onChange={(e) => {inputChangeHandler(e)}} />
+                <button onClick={addProduct}>Add Product</button>
+            </div>
+
+            <ul className="products" aria-label="list of your products">
+                {
+                    products.map((data, index) => (
+                        <li key={index}><span>{data}</span> <button onClick={() => {removeProduct(index)}}>X</button></li>
+                    ))
+                }
+            </ul>
         </section>
     );
 }
