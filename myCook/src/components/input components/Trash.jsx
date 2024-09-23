bbf5946cee2f56c8835da565d9509005a8684ca6
lekcik <cultcrib@gmail.com>
2024-09-23 14:02:26 +0200

    trash component added

--- myCook/src/components/input components/Trash.jsx
+++ myCook/src/components/input components/Trash.jsx
@@ -20,6 +20,13 @@ function Trash() {
         }
     }
 
+    function removeProduct(index) {
+        setReceipt((prevReceipt) => ({
+            ...prevReceipt,
+            excludeProducts: prevReceipt.excludeProducts.filter((_, i) => i !== index)
+        }));
+    }
+
     return(
         <section className="trash products container">
             <h2>What would you like NOT to eat?</h2>
@@ -31,6 +38,15 @@ function Trash() {
                 />
                 <button type="submit">Add Product</button> 
             </form>
+
+            <ul className="products" aria-label="list of products you don't eat">
+                {receipt.excludeProducts.map((data, index) => (
+                    <li key={index}>
+                        <button onClick={() => {removeProduct(index)}}>X</button>
+                        <span>{data}</span> 
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 }
