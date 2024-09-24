dfa10546c14a532a2ea3b9d2f5a0a389874852d6
lekcik <cultcrib@gmail.com>
2024-09-24 17:29:17 +0200

    prefs added

--- myCook/src/components/input components/Preferences.jsx
+++ myCook/src/components/input components/Preferences.jsx
@@ -6,6 +6,9 @@ function Preferences() {
     const cuisines = useRef([
         'African', 'Asian', 'American', 'British', 'Cajun', 'Caribbean', 'Chinese', 'Eastern European', 'European', 'French', 'German', 'Greek', 'Indian', 'Irish', 'Italian', 'Japanese', 'Jewish', 'Korean', 'Latin American', 'Mediterranean', 'Mexican', 'Middle Eastern', 'Nordic', 'Southern', 'Spanish', 'Thai', 'Vietnamese' 
     ]);
+    const diets = useRef([
+        'Gluten Free', 'Ketogenic', 'Vegetarian', 'Lacto-Vegetarian', 'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Primal', 'Low FODMAP', 'Whole30'
+    ]);
 
     function cuisineChangeHandler(event) {
         setReceipt((prevReceipt) => ({
@@ -14,11 +17,29 @@ function Preferences() {
         }));
     }
 
+    function dietChangeHandler(event) {
+        setReceipt((prevReceipt) => ({
+            ...prevReceipt,
+            diet: event.target.value
+        }))
+    }
+
+    function instructionsChangeHandler(event) {
+        setReceipt((prevReceipt) => ({
+            ...prevReceipt,
+            instructionsRequired: event.target.value
+        }))
+    }
+
+    function test(event) {
+        console.log(event.target.id);
+    }
+
     return(
         <section className="preferences container">
             <h2>Preferences</h2>
 
-            <form aria-label="your receipt preferences">
+            <form onChange={test} aria-label="your receipt preferences">
                 <div>
                     <h3>Cuisine</h3>
                     <select value={receipt.cuisine} onChange={cuisineChangeHandler} name="cuisines" id="cuisines">
@@ -33,10 +54,21 @@ function Preferences() {
                 
                 <div>
                     <h3>Diet</h3>
-                    <select name="diets" id="diets">
+                    <select value={receipt.diet} onChange={dietChangeHandler} name="diets" id="diets">
+                        <option value={''} key={0}>No preference</option>
                         {
-
+                            diets.current.map((data, index) => (
+                                <option value={data} key={index+1}>{data}</option>
+                            ))
                         }
+                    </select>
+                </div>
+
+                <div>
+                    <h3>Instructions required?</h3>
+                    <select value={receipt.instructionsRequired} onChange={instructionsChangeHandler} name="instructions" id="instructions">
+                        <option value={false}>No</option>
+                        <option value={true}>Yes</option>
                     </select>
                 </div>
             </form>
